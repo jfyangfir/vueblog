465e5f0b6a4a6d6ce706875b3a473d2099312f37
Your Name <you@example.com>
2020-11-16 10:37:57 +0800

    "sql"

--- src/main/resources/blog.sql
+++ src/main/resources/blog.sql
@@ -1,52 +1,56 @@
 /*
- Navicat Premium Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : mysql
- Source Server Type    : MySQL
- Source Server Version : 80021
- Source Host           : localhost:3306
- Source Schema         : blog
+Source Server         : localhost_3306
+Source Server Version : 80013
+Source Host           : localhost:3306
+Source Database       : blog
 
- Target Server Type    : MySQL
- Target Server Version : 80021
- File Encoding         : 65001
+Target Server Type    : MYSQL
+Target Server Version : 80013
+File Encoding         : 65001
 
- Date: 16/11/2020 08:55:52
+Date: 2020-11-16 10:36:36
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for my_blog
--- ----------------------------
-DROP TABLE IF EXISTS `my_blog`;
-CREATE TABLE `my_blog`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(0) NOT NULL,
-  `titlel` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
-  `created` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0),
-  `status` tinyint(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for my_user
 -- ----------------------------
 DROP TABLE IF EXISTS `my_user`;
-CREATE TABLE `my_user`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT,
-  `username` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `email` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `password` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `status` int(0) NOT NULL,
-  `created` datetime(0) NULL DEFAULT NULL,
-  `last_login` datetime(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `UK_USERNAME`(`username`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `my_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `password` varchar(64) DEFAULT NULL,
+  `status` int(5) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `UK_USERNAME` (`username`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-SET FOREIGN_KEY_CHECKS = 1;
+-- ----------------------------
+-- Records of my_user
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for m_blog
+-- ----------------------------
+DROP TABLE IF EXISTS `m_blog`;
+CREATE TABLE `m_blog` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `titlel` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `content` longtext,
+  `created` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `status` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of m_blog
+-- ----------------------------
